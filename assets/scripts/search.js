55abdacda3f5054a7a003a1574d939352f9b1147
Jake <j.t.cravey1991@gmail.com>
2020-05-11 18:08:06 -0700

    added delete functionality to ingredient list, and local storage capabilities to ingredient list and allergies

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -1,8 +1,31 @@
 jQuery.noConflict();
 
 var ingredientList = [];
+var allergies = {
+    dairy: false,
+    eggs: false,
+    gluten: false,
+    peanut: false,
+    treeNut: false,
+    seafood: false,
+    shellfish: false,
+    soy: false
+};
 
+var ingredientElement = document.getElementById("currentIngredientsList");
+var allergyChecks = document.getElementById("allergiesForm");
 
+initialize();
+
+// initializes page with saved ingredients and allergies
+function initialize() {
+    loadIngredientList();
+    loadAllergies();
+    renderIngredients();
+    renderAllergies();
+}
+
+// event listener for add ingredient button
 (function ($) {
     $("addButton").addEvent("click", function (event) {
         event.preventDefault();
@@ -11,6 +34,25 @@ var ingredientList = [];
     });
 })(document.id);
 
+// event listener for ingredients delete button
+ingredientElement.addEventListener("click", function (event) {
+    if (event.target.type === "button") {
+        var index = event.target.id;
+        ingredientList.splice(index, 1);
+        saveIngredientList();
+        renderIngredients();
+    }
+});
+
+// event listener for allergy check boxes
+allergyChecks.addEventListener("click", function (event) {
+    if (event.target.type === "checkbox") {
+        updateAllergies();
+        saveAllergies();
+    }
+});
+
+// submits an ajax to check if added ingredient is supported
 function checkIngredient(term) {
     var checkedIngredient = term.toLowerCase();
     jQuery.ajax({
@@ -27,11 +69,13 @@ function checkIngredient(term) {
         else {
             ingredientList.push(document.id("addIngredientInput").value);
             document.id("addIngredientInput").value = "";
+            saveIngredientList();
             renderIngredients();
         }
     })
 }
 
+// lets the user know the ingredient they tried to add wasnt found
 function wrongIngredient() {
     var $ = document.id;
 
@@ -42,6 +86,7 @@ function wrongIngredient() {
     }, 2000)
 }
 
+// renders the ingredient list with delete buttons
 function renderIngredients() {
     document.id("currentIngredientsList").empty();
     for (var i = 0; i < ingredientList.length; i++) {
@@ -51,8 +96,63 @@ function renderIngredients() {
         var deleteButton = new Element("button");
         deleteButton.textContent = "delete";
         deleteButton.id = i;
+        deleteButton.set("type", "button");
 
         li.grab(deleteButton);
         document.id("currentIngredientsList").grab(li);
     }
+}
+
+// saves current ingredient list to local storage
+function saveIngredientList() {
+    localStorage.setItem("ingredientList", JSON.stringify(ingredientList));
+}
+
+// loads ingredient list from local storage
+function loadIngredientList() {
+    var tempList = JSON.parse(localStorage.getItem("ingredientList"));
+    if (tempList !== null) {
+        ingredientList = tempList;
+    }
+}
+
+//saves current allergy object to local storage
+function saveAllergies() {
+    localStorage.setItem("allergies", JSON.stringify(allergies));
+}
+
+// loads allergy object from local storage
+function loadAllergies() {
+    var tempAllergies = JSON.parse(localStorage.getItem("allergies"));
+    if (tempAllergies !== null) {
+        allergies = tempAllergies;
+    }
+}
+
+// updates allergy check boxes to allergy object
+function renderAllergies() {
+    var $ = document.id;
+
+    $("dairyBox").checked = allergies.dairy;
+    $("eggBox").checked = allergies.eggs;
+    $("glutenBox").checked = allergies.gluten;
+    $("peanutBox").checked = allergies.peanut;
+    $("treeNutBox").checked = allergies.treeNut;
+    $("seafoodBox").checked = allergies.seafood;
+    $("shellfishBox").checked = allergies.shellfish;
+    $("soyBox").checked = allergies.soy;
+}
+
+// updates allergy object to what is checked on screen
+function updateAllergies() {
+    var $ = document.id;
+
+    allergies.dairy = $("dairyBox").checked;
+    allergies.eggs = $("eggBox").checked;
+    allergies.gluten = $("glutenBox").checked;
+    allergies.peanut = $("peanutBox").checked;
+    allergies.treeNut = $("treeNutBox").checked;
+    allergies.seafood = $("seafoodBox").checked;
+    allergies.shellfish = $("shellfishBox").checked;
+    allergies.soy = $("soyBox").checked;
 }
