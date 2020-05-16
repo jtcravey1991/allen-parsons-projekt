e27e690b4c95e83b6c1ddecae0ffbdbdc395c927
RadioJackie <jacobpeters628@gmail.com>
2020-05-15 22:43:37 -0700

    this is a useless push don't merge

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -148,36 +148,7 @@ jQuery(".inner-switch").on("click", function () {
 });
 
 /*
-var setTheme = function (theme) {
-    if (theme === 'dark') {
-        // dark
-        $("body").removeClass("standard");
-        $("body").addClass("dark");
-        $(".inner-switch").text("ON");
-        setCookie('Theme', 'dark', 30);
-    } else {
-        $("body").removeClass("dark");
-        $("body").addClass("standard");
-        $(".inner-switch").text("OFF");
-        setCookie('Theme', 'standard', 30);
-    }
-};
 
-currentTheme = getCookie('Theme');
-setTheme(currentTheme);
-
-$(".inner-switch").on("click", function () {
-    if ($("body").hasClass("dark")) {
-        // standard
-        setTheme('standard');
-    } else {
-        // dark mode
-        setTheme('dark');
-    }
-});
-
-var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-return v ? v[2] : null;
 
 
 function setCookie(name, value, days) {
